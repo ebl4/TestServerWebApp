110a941bcaa4ad8880adf24cbf3139d2d132b66c
Edson <Edson@DESKTOP-D0BM5A4>
2022-11-21 20:55:27 -0300

    Adicionar arquivos de projeto.

---
+++ TestServerSample/PrimeWebDefaultRequestShould.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestServerWebApp
+{
+    public class PrimeWebDefaultRequestShould
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+        public PrimeWebDefaultRequestShould()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder());
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnHelloWorld()
+        {
+            // Act
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            // Assert
+            Assert.Equal("Hello World!", responseString);
+        }
+    }
+}
---
+++ TestServerSample/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TestServerSample
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
